0729ce14e3a1ce0a6ff5b6da9ab618f38d745a9c
Shinmera <shinmera@tymoon.eu>
2023-03-21 10:37:59 +0100

    Minor

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Assets for the Trial game engine.
 You will need to have [Git LFS](https://git-lfs.github.com/) installed in order to access the resources in this repository.
 
 After cloning the repository, run 'git lfs install', 'git lfs fetch' and 'git lfs checkout' to properly set up the data files and tracking.
+
+To use assets from this, you'll probably want to use a local-nickname like `trial-assets` and then use the functions `//` and `asset`:
+
+```commonlisp
+(trial-assets:// :heart-in-the-sand)
+```
+
+These are analogous to Trial's versions, except that it automatically coerces the name to be within the trial-assets package and prefixing the `pool`, reducing redundancy.
--- assets.lisp
+++ assets.lisp
@@ -1,9 +1,45 @@
 (defpackage #:org.shirakumo.fraf.trial.assets
-  (:use #:cl)
-  (:local-nicknames (#:trial #:org.shirakumo.fraf.trial))
+  (:use #:cl+trial)
+  (:shadow #:// #:asset)
   (:export
-   #:pool))
+   #:pool
+   #://
+   #:asset
+   #:heart-in-the-sand
+   #:masko-naive
+   #:nissi-beach))
 
 (in-package #:org.shirakumo.fraf.trial.assets)
 
-(trial:define-pool pool)
+(define-pool pool)
+
+(defun // (name &optional (resource T))
+  (trial:// 'pool (intern (string name) #.*package*) resource))
+
+(define-compiler-macro // (name &optional (resource T) &environment env)
+  (if (constantp name env)
+      `(trial:// 'pool ',(intern (string name) #.*package*) ,resource)
+      `(trial:// 'pool (intern ,(string name) #.*package*) ,resource)))
+
+(defun asset (name &optional errorp)
+  (trial:asset 'pool (intern (string name) #.*package*) errorp))
+
+(define-compiler-macro asset (name &optional errorp &environment env)
+  (if (constantp name env)
+      `(trial:asset 'pool ',(intern (string name) #.*package*) ,errorp)
+      `(trial:asset 'pool (intern ,(string name) #.*package*) ,errorp)))
+
+(define-asset (pool heart-in-the-sand) image
+    '(#p"heart-in-the-sand/posx.jpg" #p"heart-in-the-sand/negx.jpg"
+      #p"heart-in-the-sand/posy.jpg" #p"heart-in-the-sand/negy.jpg"
+      #p"heart-in-the-sand/posz.jpg" #p"heart-in-the-sand/negz.jpg"))
+
+(define-asset (pool masko-naive) image
+    '(#p"masko-naive/posx.jpg" #p"masko-naive/negx.jpg"
+      #p"masko-naive/posy.jpg" #p"masko-naive/negy.jpg"
+      #p"masko-naive/posz.jpg" #p"masko-naive/negz.jpg"))
+
+(define-asset (pool nissi-beach) image
+    '(#p"nissi-beach/posx.jpg" #p"nissi-beach/negx.jpg"
+      #p"nissi-beach/posy.jpg" #p"nissi-beach/negy.jpg"
+      #p"nissi-beach/posz.jpg" #p"nissi-beach/negz.jpg"))
