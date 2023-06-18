64d7b06d6c9f83f8241babe93c84a91cf46a5dac
Shinmera <shinmera@tymoon.eu>
2023-06-18 19:43:55 +0200

    Minor

--- assets-gltf.lisp
+++ assets-gltf.lisp
@@ -1,4 +1,7 @@
 (in-package #:org.shirakumo.fraf.trial.assets)
 
 (define-asset (pool woman) org.shirakumo.fraf.trial.gltf:asset
-    #p "Woman.gltf")
+    #p "woman.glb")
+
+(define-asset (pool marble-bust) org.shirakumo.fraf.trial.gltf:asset
+    #p "marble-bust.glb")
